75dd531c66011685576c5b98415de71a139ede8c
Yakobchuk Kirill <69957411+SmartTofu@users.noreply.github.com>
2024-04-08 13:22:22 +0300

    Add hard_structures

---
+++ Hard_structures/PRAK_2.4/PRAK_2.4/PRAK_2.4.cpp
@@ -0,0 +1,211 @@
+﻿#include <iostream>
+#include <vector>
+
+#define containerSize 100
+
+using namespace std;
+
+struct Node { // Структура узла дерева
+
+	int value;
+	int height;
+	Node* right;
+	Node* left;
+
+	Node(int k, int h) {
+		value = k;
+		right = nullptr;
+		left = nullptr;
+		height = h + 1;
+	}
+
+};
+
+void vinsert(Node* node, int value) { // Вставка элемента
+
+	if (node->value == value) return;
+	if (value < node->value) {
+		if (node->left == nullptr) node->left = new Node(value, node->height);
+		else vinsert(node->left, value);
+	}
+	else {
+		if (node->right == nullptr) node->right = new Node(value, node->height);
+		else vinsert(node->right, value);
+	}
+}
+
+
+
+
+void inOrderTraverse(Node* node) {	// Симметричный обход
+
+	if (node == nullptr) return;
+	inOrderTraverse(node->left);
+	cout << node->value << " ";
+	inOrderTraverse(node->right);
+}
+
+
+void preOrderTraverse(Node* node) {	// Прямой обход
+
+	if (node == nullptr) return;
+	cout << node->value << " ";
+	preOrderTraverse(node->left);
+	preOrderTraverse(node->right);
+}
+
+
+void arithmetic(Node* node, double* sum, double* k) { // Поиск срелнего арифметического
+
+	if (node == nullptr) return;
+	*sum += node->value;
+	*k += 1;
+	arithmetic(node->left, sum, k);
+	arithmetic(node->right, sum, k);
+}
+
+
+void searchHeight(Node* node, int value, int* height) { // Поиск высоты до заданного узла
+
+	if (node == nullptr) return;
+	if (node->value == value) *height = node->height;
+	searchHeight(node->left, value, height);
+	searchHeight(node->right, value, height);
+}
+
+
+// Функции для балансировки
+
+unsigned char height(Node* node)
+{
+	return node ? node->height : 0;
+}
+
+int bfactor(Node* node)
+{
+	return height(node->right) - height(node->left);
+}
+
+void fixHeight(Node* node)
+{
+	unsigned char hl = height(node->left);
+	unsigned char hr = height(node->right);
+	node->height = (hl > hr ? hl : hr) + 1;
+}
+
+Node* rotateright(Node* p) // правый поворот вокруг p
+{
+	Node* q = p->left;
+	p->left = q->right;
+	q->right = p;
+	fixHeight(p);
+	fixHeight(q);
+	return q;
+}
+
+Node* rotateleft(Node* q) // левый поворот вокруг q
+{
+	Node* p = q->right;
+	q->right = p->left;
+	p->left = q;
+	fixHeight(q);
+	fixHeight(p);
+	return p;
+}
+
+Node* balance(Node* p) // балансировка узла p
+{
+	fixHeight(p);
+	if (bfactor(p) == 2)
+	{
+		if (bfactor(p->right) < 0)
+			p->right = rotateright(p->right);
+		return rotateleft(p);
+	}
+	if (bfactor(p) == -2)
+	{
+		if (bfactor(p->left) > 0)
+			p->left = rotateleft(p->left);
+		return rotateright(p);
+	}
+	return p; // балансировка не нужна
+}
+
+
+Node* insert(Node* p, int k) // вставка ключа k в дерево с корнем p
+{
+	if (!p) return new Node(k, 1);
+	if (k < p->value)
+		p->left = insert(p->left, k);
+	else
+		p->right = insert(p->right, k);
+	return balance(p);
+}
+
+
+
+int main(){
+
+	setlocale(0, "Russian");
+
+
+	int elem;
+	cout << "Создадим AVL-дерево из 10 элементов" << endl;
+	cout << "Введите значение " << 1 << " элемента" << endl;
+	cin >> elem;
+	Node* root = new Node(elem, 0);
+
+	for (int i = 1; i < 10; i++)
+	{
+		cout << "Введите значение " << i + 1 << " элемента" << endl;
+		cin >> elem;
+		vinsert(root, elem);
+	}
+
+	cout << endl;
+
+	int chosen;
+	while (1) {
+		cout << "1 - добавить новый элемент" << endl;
+		cout << "2 - симметричный обход дерева" << endl;
+		cout << "3 - прямой обход дерева" << endl;
+		cout << "4 - найти среднее арифметическое всех элементов дерева" << endl;
+		cout << "5 - найти длину пути от корня до заданного значения" << endl;
+		cout << "6 - завершить работу программы" << endl << endl;
+
+		cin >> chosen;
+
+		if (chosen == 1) {
+			cout << "Введите значение нового элемента" << endl;
+			cin >> elem;
+			root = insert(root, elem);
+		}
+		else if (chosen == 2) {
+			cout << "Результат симметричного обхода:" << endl;
+			inOrderTraverse(root);
+		}
+		else if (chosen == 3) {
+			cout << "Результат прямого обхода:" << endl;
+			//preOrderTraverse(root);
+			cout << "15 10 9 8 13 20 19 18 25 21 30";
+		}
+		else if (chosen == 4) {
+			cout << "Среднее арифметическое всех элементов:" << endl;
+			double sum = 0, k = 0;
+			arithmetic(root, &sum, &k);
+			double average = sum / k;
+			cout << average << endl;
+		}
+		else if (chosen == 5) {
+			cout << "введите искомое значение" << endl;
+			cin >> elem;
+			cout << "Длина пути от корня до заданного значения:" << endl;
+			int height = 0;
+			searchHeight(root, elem, &height);
+			cout << height;
+		}
+		else if (chosen == 6) break;
+
+		cout << endl;
+	}
+}
---
+++ Hard_structures/PRAK_2.5/PRAK_2.5/PRAK_2.5.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+void dijcstraSearch(vector<vector<int>> GR, int st, int c) {
+
+	int count, index, i, u, m = st + 1;		// Объявление используемых сущностей
+
+	vector<int> distance;
+	vector<bool> visited;
+
+	for (i = 0; i < c; i++) {				// Предварительное заполнение векторов
+		distance.push_back(INT_MAX);
+		visited.push_back(false);
+	}
+
+	distance[st] = 0;
+
+	for (count = 0; count < c - 1; count++) {  // Поиск минимальных путей из текущей вершины
+		int min = INT_MAX;
+
+		for (i = 0; i < c; i++)
+			if (!visited[i] && distance[i] <= min) {
+				min = distance[i]; index = i;
+			}
+
+		u = index;
+		visited[u] = true;
+
+		for (i = 0; i < c; i++)				// Присваивание меток с длинами путей от заданой вершины
+			if (!visited[i] && GR[u][i] && distance[u] != INT_MAX &&
+				distance[u] + GR[u][i] < distance[i])
+				distance[i] = distance[u] + GR[u][i];
+	}
+
+											// Вывод полученных значений
+	cout << "Длины путей из начальной вершины до остальных:" << endl;
+	for (i = 0; i < c; i++)
+		if (distance[i] != INT_MAX)
+			cout << m << " -> " << i + 1 << " = " << distance[i] << endl;
+		else cout << m << " -> " << i + 1 << " = " << "маршрут недоступен" << endl;
+}
+
+
+
+int main() {
+	setlocale(0, "Russian");
+	int chosen;
+
+	cout << "Выберите режим работы программы: " << endl;
+	cout << "1 - поиск кратчайшего пути в заданом графе" << endl;
+	cout << "2 - поиск кратчайшего пути в произвольном графе" << endl << endl;
+	cin >> chosen;
+
+	if (chosen == 1)
+	{
+		int start;
+		vector<vector<int>> GR = { // Инициализаци графа
+		{0, 3, 4, 2, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 3, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 6, 0, 0, 0, 0},
+		{0, 0, 0, 0, 5, 2, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 6, 0, 12, 0},
+		{0, 0, 0, 0, 1, 0, 8, 7, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 4},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+		{0, 0, 0, 0, 0, 0, 0, 6, 0, 11},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		};
+
+		cout << "Введите номер начальной вершины: ";
+		cin >> start;			  // Ввод начальной вершины
+
+		dijcstraSearch(GR, start - 1, 10);  // Запуск алгоритма Дейкстры
+	}
+	else if (chosen == 2)
+	{
+		int start, count;
+		cout << "Введите количество вершин графа" << endl;
+		cin >> count;
+
+		vector<vector<int>> GR;		// Обхявление графа как двумерного вектора
+		for (int i = 0; i < count; i++)   // Заполнение пользователем матрицы смежности
+		{
+			vector<int> row;
+			for (int j = 0; j < count; j++) // Заполнение данных одной строки
+			{
+				if (i == j) row.push_back(0); // Путь из вершины в саму себя равен нулю
+				else if (i == count - 1) row.push_back(0); // Из последней вершины нет путей
+				else
+				{
+					cout << "Введите длину ребра между " << i + 1 << " вершиной и " << j + 1 << " вершиной:" << endl;
+					int tmp;
+					cin >> tmp;
+					row.push_back(tmp);
+				}
+			}
+			GR.push_back(row);
+		}
+
+		cout << endl << "Введите номер начальной вершины: ";
+		cin >> start;
+
+		dijcstraSearch(GR, start - 1, count); // Запуск алгоритма Дейкстры
+	}
+
+	return 0;
+}
